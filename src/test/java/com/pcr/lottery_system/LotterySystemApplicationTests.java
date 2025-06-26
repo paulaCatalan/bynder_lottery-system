4b51540ee680b546f98a208c7012f97b05134771
Paula Catalán <paulacatalan19@gmailcom>
2025-06-26 18:13:53 +0200

    ✨➕ Create Spring Boot project with Spring initializr

---
+++ src/test/java/com/pcr/lottery_system/LotterySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcr.lottery_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotterySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
